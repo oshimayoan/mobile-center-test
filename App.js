44a4efc0a19df7e8a4af6e449822f6d11a0975cd
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2017-09-11 15:57:59 +0700

    Add minor text

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +8,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Text>This app is built by mobile center.</Text>
       </View>
     );
   }
